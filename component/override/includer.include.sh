895f3c63206ec588d8b3d631978810fb1ee8bde3
Rich Moyse <rich@moyse.us>
2017-12-10 00:49:16 -0500

    make reference relative to include.sh main script, not to load from  includer.includes.sh

--- component/override/includer.include.sh
+++ component/override/includer.include.sh
@@ -3,4 +3,4 @@
 # a lesser implementation to aggregate components as the component aggregater
 # boots itself.  the below loads the base and permits overriding its contents
 # without touching the base implementation (Open/Close) principle of SOLID
-source "$(dirname "${BASH_SOURCE[0]}")"/base/includer.include.sh 
+source "$(dirname "${BASH_SOURCE[1]}")"/base/includer.include.sh 
