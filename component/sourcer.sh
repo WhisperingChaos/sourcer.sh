522007a1946e5356e94e106cb0efb9eca2cde054
Rich Moyse <Rich@Moyse.US>
2018-01-29 10:23:36 -0500

    Update sourcer.sh
    fix grammar in comments

--- component/sourcer.sh
+++ component/sourcer.sh
@@ -2,13 +2,15 @@
 ###############################################################################
 #
 #	Purpose:
-#		Thunk layer to construct an aggregrate "component" through hierarchical
+#		Thunk layer to construct an aggregrate "component" through composition of
+#   components arranged in a hierarchy.
+#
 #		see sourcer.source.sh for implementation.
 #
 ###############################################################################
 
 # compose source files from override directory to permit overrides for
-# itself via different mechanism when boot stratping itself
+# itself via different mechanism when boot straping itself
 source "$(dirname "${BASH_SOURCE[0]}")"/override/sourcer.source.sh 
 #compose specified component
 sourcer_compose "$1"
