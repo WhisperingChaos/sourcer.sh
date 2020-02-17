006b7196668db87598bdce2910c3393d4097cc8f
Rich Moyse <rich@moyse.us>
2020-02-16 21:10:41 -0500

    Eliminated override technique by default. Just have developer manually override - reduce complexity for something that is not likely to change

--- component/override/sourcer.source.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# a lesser implementation to aggregate components as the component aggregater
-# boots itself.  the below loads the base and permits overriding its contents
-# without touching the base implementation (Open/Close) principle of SOLID
-source "$(dirname "${BASH_SOURCE[1]}")"/base/sourcer.source.sh 
