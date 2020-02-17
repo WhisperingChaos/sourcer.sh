a9a6809d5c0739635c246723ef4147fded474b1a
Rich Moyse <rich@moyse.us>
2020-02-16 21:34:15 -0500

    improved purpose, can now be soft linked from anywhereand it will correctly resolve its internal components

--- component/sourcer.sh
+++ component/sourcer.sh
@@ -2,15 +2,27 @@
 ###############################################################################
 #
 #	Purpose:
-#		Thunk layer to construct an aggregrate "component" through composition of
-#   components arranged in a hierarchy.
-#
-#		see sourcer.source.sh for implementation.
+#		Manually, statically compose sourcer so it can automatically,
+#		dynamically compose other components.  Don't call this executable
+#		component directly. Instead, insert at top of component being built
+#		the command: 'source [<path>]sourcer.build.source.sh [<parameters>]'
+#		along with appropriate <parameters>.  Use '--help' to determine
+#		parameter values: 'source [<path>]sourcer.build.source.sh --help' 
 #
 ###############################################################################
 
-# compose source files from override directory to permit overrides for
-# itself via different mechanism when boot straping itself
-source "$(dirname "${BASH_SOURCE[0]}")"/override/sourcer.source.sh 
-#compose specified component
-sourcer_compose "$1"
+main(){
+	# identify the actual directory location of this executable it's components
+	# will always be in the 'base' subdirectory of this one.
+	declare -r sourcer__EXECUTABLE_DIR="$(dirname "$(readlink -f "$0")")"
+	source "$sourcer__EXECUTABLE_DIR/base/sourcer.source.sh"
+	# manual overrides of implementation should be placed in 'override'
+	if [ -e "$sourcer__EXECUTABLE_DIR/override/sourcer.source.sh" ]; then
+		source "$sourcer__EXECUTABLE_DIR/override/sourcer.source.sh"
+	fi
+
+	sourcer_compose "$1"
+}
+
+main $@
+
